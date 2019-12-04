b0e9e5629756052d3f07b266e15cc481c486499d
Alex Kalkatos <alexcsts@gmail.com>
2019-12-04 07:50:54 -0300

    Card basic implementation
    Card basic implementation

---
+++ Cardgame Framework/Assets/Scripts/CardData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardData : ScriptableObject
+{
+	public string id;
+	public string variety;
+	public CardField[] fields;
+}
---
+++ Cardgame Framework/Assets/Scripts/CardField.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InfoType
+{
+	Boolean,
+	String,
+	Number,
+	Image
+}
+
+public struct CardField
+{
+	public string fieldName;
+	public InfoType type;
+	public bool booleanInfo;
+	public string stringInfo;
+	public double numberInfo;
+	public Sprite imageInfo;
+}
