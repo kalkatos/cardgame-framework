a13ba38d1f4bcb426e7a9a14b908516c80469e02
Alex Kalkatos <alexcsts@gmail.com>
2020-01-10 10:51:55 -0300

    Revert "Revert "Fusing MatchSceneManager(extinct) into CGEngineManager""
    This reverts commit d2123e27147eaba5b979cd12c36151251db39eec.

--- Cardgame Framework/Assets/CGEngine/Scripts/Classes/Player.cs
+++ Cardgame Framework/Assets/CGEngine/Scripts/Classes/Player.cs
@@ -24,7 +24,6 @@ namespace CGEngine
 
 		public UserType userType;
 		public PlayerRules playerRules;
-		public Deck deck;
 		public string id; //starts with “p”
 		public string actionChosen;
 		public Card cardChosen;
