7a620493843ccfeafe16293a1e19205ff5b303a2
Alex Kalkatos <alexcsts@gmail.com>
2020-05-02 11:21:51 -0300

    v0.1.78 - UIElements breakdown
    v0.1.78 - UIElements breakdown

---
+++ Cardgame Framework/Assets/CGEngine/Scripts/Managers/ActionSender.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class ActionSender : MonoBehaviour
+{
+    public void SendAction (string actionName)
+	{
+		
+	}
+}
